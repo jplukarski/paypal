4c3579688ef5f6d3f1fbe302901082cc9a0665f7
mkirlin <mkirlin@gmail.com>
2021-10-06 19:02:44 +0000

    Deploying to gh-pages from  @ ad227817ad8b2ffe1d56a9e1317644dcbe3ac235 🚀

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ paypal.Buttons({
         console.log('Something went wrong', err);
     }
 }).render('#paypal-button-container'); // Display payment options on your web page
+
+paypal.Buttons({
+    style: {
+        shape: 'rect',
+        color: 'gold',
+        layout: 'vertical',
+        label: 'subscribe'
+    },
+    createSubscription: function (data, actions) {
+        return actions.subscription.create({
+            /* Creates the subscription */
+            plan_id: 'P-0U385723JC662250XMFO6LII'
+        });
+    },
+    onApprove: function (data, actions) {
+        alert(data.subscriptionID); // You can add optional success message for the subscriber here
+    }
+}).render('#paypal-button-container-P-0U385723JC662250XMFO6LII'); // Renders the PayPal button
