e244fa1c414e1faee5eca2af5f5f46a751b983cb
jwplukarski <jwplukarski@gmail.com>
2022-07-07 10:55:51 -0500

    single card fields demo

--- client/index.js
+++ client/index.js
@@ -1,21 +1,56 @@
 /* eslint-disable consistent-return, new-cap, no-alert, no-console */
 
-paypal.Buttons({
-    createOrder: function (data, actions) {
-        return actions.order.create({
-            purchase_units: [{
-                amount: {
-                    value: '50.00'
-                }
-            }]
-        });
-    },
-    onApprove: function (data, actions) {
-        return actions.order.capture().then(function (details) {
-            window.location.href = "approve.html";
-        });
-    },
-    onError: function (err) {
-        console.log('Something went wrong', err);
-    }
-}).render('#paypal-button-container'); // Display payment options on your web page
+const singleCardFieldButton = document.getElementById("single-card-field-button");
+
+const styleObject = {
+  input: {
+    "font-size": "16 px",
+    "font-family": "monospace",
+    "font-weight": "lighter",
+    color: "blue",
+    "box-shadow": "10px 5px 5px red",
+  }
+};
+
+const cardField = paypal.CardFields({
+  // style: styleObject,
+  createOrder: function (data, actions) {
+    console.log("Here are the actions", actions);
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "50.00",
+          },
+        },
+      ],
+    });
+    // return fetch('/api/paypal/order/create/', {
+    //     method: 'post'
+    // }).then((res) => {
+    //     return res.json();
+    // }).then((orderData) => {
+    //     console.log('Order: ', orderData)
+    //     return orderData.id;
+    // });
+  },
+  onApprove: function (data, actions) {
+    console.log("on approve happened");
+    return actions.order.capture().then(function (details) {
+      window.location.href = "approve.html";
+    });
+  },
+});
+
+cardField.render("#paypal-card-field-container");
+
+singleCardFieldButton.addEventListener("click", () => {
+  cardField
+    .submit()
+    .then(() => {
+      console.log("single card fields submit");
+    })
+    .catch((err) => {
+      console.log("There was an error with single card fields: ", err);
+    });
+});
