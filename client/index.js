6f0ac74868a0bffdc3960f7ec057b0751b1516ab
jwplukarski <jwplukarski@gmail.com>
2022-07-07 10:22:56 -0500

    multi-card-fields demo

--- client/index.js
+++ client/index.js
@@ -1,21 +1,73 @@
 /* eslint-disable consistent-return, new-cap, no-alert, no-console */
 
-paypal.Buttons({
-    createOrder: function (data, actions) {
-        return actions.order.create({
-            purchase_units: [{
-                amount: {
-                    value: '50.00'
-                }
-            }]
-        });
-    },
-    onApprove: function (data, actions) {
-        return actions.order.capture().then(function (details) {
-            window.location.href = "approve.html";
-        });
-    },
-    onError: function (err) {
-        console.log('Something went wrong', err);
-    }
-}).render('#paypal-button-container'); // Display payment options on your web page
+const cardNameContainer = document.getElementById("card-name-field-container"); // Optional field
+const cardNumberContainer = document.getElementById(
+  "card-number-field-container"
+);
+const cardCvvContainer = document.getElementById("card-cvv-field-container");
+const cardExpiryContainer = document.getElementById(
+  "card-expiry-field-container"
+);
+const cardPostalCodeContainer = document.getElementById("card-postal-code-field-container");
+const multiCardFieldButton = document.getElementById("multi-card-field-button");
+
+const styleObject = {
+  input: {
+    "font-size": "16 px",
+    "font-family": "monospace",
+    "font-weight": "lighter",
+    color: "blue",
+    "box-shadow": "10px 5px 5px red",
+  },
+  ".invalid": {
+    color: "red",
+  },
+};
+
+const cardField = paypal.CardFields({
+  // style: styleObject,
+  createOrder: function (data, actions) {
+    console.log("Here are the actions", actions);
+    // return actions.order.create({
+    //   purchase_units: [
+    //     {
+    //       amount: {
+    //         value: "50.00",
+    //       },
+    //     },
+    //   ],
+    // });
+    return fetch('/api/paypal/order/create/', {
+        method: 'post'
+    }).then((res) => {
+        return res.json();
+    }).then((orderData) => {
+        console.log('Order: ', orderData)
+        return orderData.id;
+    });
+  },
+  onChange: ({ isValid, errors }) => {
+    console.log("onchange: ", isValid, errors);
+  },
+  onApprove: function (data, actions) {
+    console.log("on approve happened");
+    return actions.order.capture().then(function (details) {
+      window.location.href = "approve.html";
+    });
+  },
+});
+
+cardField.NameField().render(cardNameContainer);
+cardField.NumberField().render(cardNumberContainer);
+cardField.CVVField().render(cardCvvContainer);
+cardField.ExpiryField().render(cardExpiryContainer);
+cardField.PostalCodeField().render(cardPostalCodeContainer)
+
+multiCardFieldButton.addEventListener('click', () => {
+    console.log('clicked multi fields button')
+    cardField.submit().then(()=>{
+        console.log('multi card fields submit')
+    }).catch((err) => {
+        console.log("There was an error with multi card fields: ", err)
+    })
+});
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const { resolve } = require("path")
+const fetch = require("node-fetch");
 
 const app = express()
 
@@ -8,5 +9,62 @@ app.use(express.static(resolve(__dirname, "../client")))
 app.get("/", (req, res) => {
   res.sendFile(resolve(__dirname, "../client/index.html"));
 });
+
+// const CLIENT_ID = 'alc_client1'
+// const APP_SECRET = 'secret'
+// const CLIENT_ID = 'B_AF7iNYDLbxkdU12xnJWgVZDmfKv7khjdgZ6mMG89YNVD2mH3dE-ypPQxh3WW8eDYSUJUOY0i__WTicdo'
+// const APP_SECRET = 'ENzQHG7oxCna3MsFKqtSx1Vr1fyRIHPfFtnZ4efurqYARwOttdIB_MRslf45l_DA4I1MQz8afW6SFAQf'
+ 
+const CLIENT_ID = 'B_A3Nk8jo-3tRnmAWKP1vmp7hhd4oSTLUSzyS3A3WvmOPWc2syHRjoA68GmQpjiPVc3X2sexAwMQaNn2Lk'
+const APP_SECRET = 'EEF_V0V35vFQl_M7nDSDLBevD_ptRnRmcgZrRBlpac7Vpdz9J0_aCDa5pINFlOXMwe1vfXkccQeUm-uY'
+const base = "http://localhost.paypal.com:8000";
+
+(async function () {
+
+  async function generateAccessToken() {
+    const auth = Buffer.from(CLIENT_ID + ":" + APP_SECRET).toString("base64");
+    const response = await fetch(`${base}/v1/oauth2/token`, {
+      method: "post",
+      body: "grant_type=client_credentials",
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    });
+    const data = await response.json();
+    return data.access_token;
+  }
+
+  app.post("/api/paypal/order/create/", async (req, res) => {
+    // res.sendFile(resolve(__dirname, "../client/index.html"));
+    // console.log('Request', req)
+    // res.json({id: '123456789'})
+    // paypalSDK.createOrder(res)
+    console.log("Response", res)
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const response = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: "100.00",
+            },
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    console.log('Data: ', data)
+    res.json(data);
+  });
+
+})();
 
 app.listen(8080)
