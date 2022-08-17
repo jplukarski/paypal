dac1f9781c2db623eba4108fa054dd68e561e55c
jwplukarski <jwplukarski@gmail.com>
2022-08-17 14:23:13 -0500

    addd set attribute button

--- client/index.js
+++ client/index.js
@@ -8,11 +8,19 @@ const cardCvvContainer = document.getElementById("card-cvv-field-container");
 const cardExpiryContainer = document.getElementById(
   "card-expiry-field-container"
 );
-const cardPostalCodeContainer = document.getElementById("card-postal-code-field-container");
+const cardPostalCodeContainer = document.getElementById(
+  "card-postal-code-field-container"
+);
 const multiCardFieldButton = document.getElementById("multi-card-field-button");
-const singleCardFieldsContainer = document.getElementById('single-card-fields-container')
-const singleCardFieldButton = document.getElementById("single-card-field-button")
-const eligibleButton = document.getElementById("is-eligible")
+const singleCardFieldsContainer = document.getElementById(
+  "single-card-fields-container"
+);
+const singleCardFieldButton = document.getElementById(
+  "single-card-field-button"
+);
+const setAttributeButton = document.getElementById("set-attribute");
+
+const removeAttributeButton = document.getElementById("remove-attribute")
 
 const styleObject = {
   input: {
@@ -60,75 +68,86 @@ const cardField = paypal.CardFields({
   },
 });
 
-console.log(cardField)
-
-eligibleButton.addEventListener('click', () => {
-  console.log('CardField Eligibility');
-  const eligibiility = cardField.isEligible();
-  console.log('isEligible result: ',eligibiility);
-})
-
-const buttons = paypal.Buttons({
-  createOrder: function (data, actions) {
-    return actions.order.create({
-        purchase_units: [{
-            amount: {
-                value: '50.00'
-            }
-        }]
-    });
-},
-onApprove: function (data, actions) {
-    return actions.order.capture().then(function (details) {
-        window.location.href = "approve.html";
-    });
-},
-onError: function (err) {
-    console.log('Something went wrong', err);
-}
-})
-
-console.log(buttons);
-const buttonIsEligible = document.getElementById("buttons-is-eligible");
-buttonIsEligible.addEventListener('click', () => {
-    console.log('Button is Eligible Clicked')
-    const buttonEligibleResult = buttons.isEligible()
-    console.log(buttonEligibleResult);
-});
-
+// console.log('Card Field:')
+// console.log(cardField);
 cardField.render(singleCardFieldsContainer);
-cardField.NameField({  onChange: function({isValid, errors}) {
-  console.log('onchange name: ', isValid, errors)
-},}).render(cardNameContainer);
-cardField.NumberField({  onChange: function({isValid, errors}) {
-  console.log('onchange name: ', isValid, errors)
-},}).render(cardNumberContainer);
-cardField.CVVField({  onChange: function({isValid, errors}) {
-  console.log('onchange name: ', isValid, errors)
-},}).render(cardCvvContainer);
-cardField.ExpiryField({  onChange: function(data) {
-  console.log('onchange name: ', data)
-}}).render(cardExpiryContainer);
-cardField.PostalCodeField(
-  {minLength: 4, maxLength: 9,   onChange: function(data) {
-    console.log('onchange postal: ', data)
-  },}
-).render(cardPostalCodeContainer)
 
-multiCardFieldButton.addEventListener('click', () => {
-    console.log('clicked multi fields button')
-    cardField.submit().then(()=>{
-        console.log('multi card fields submit')
-    }).catch((err) => {
-        console.log("There was an error with multi card fields: ", err)
-    })
+const myNameField = cardField.NameField({
+  // onChange: function ({ isValid, errors }) {
+  //   console.log("onchange name: ", isValid, errors);
+  // },
 });
 
-singleCardFieldButton.addEventListener('click', () => {
-    console.log('clicked single fields button')
-    cardField.submit().then(()=>{
-        console.log('single card fields submit')
-    }).catch((err) => {
-        console.log("There was an error with single card fields: ", err)
+myNameField.render(cardNameContainer);
+
+const myNumberField = cardField.NumberField({
+  // onChange: function ({ isValid, errors }) {
+  //   console.log("onchange name: ", isValid, errors);
+  // },
+});
+
+myNumberField.render(cardNumberContainer);
+
+const myCVVField = cardField.CVVField({
+  // onChange: function ({ isValid, errors }) {
+  //   console.log("onchange name: ", isValid, errors);
+  // },
+});
+
+myCVVField.render(cardCvvContainer);
+
+const myExpiryField = cardField.ExpiryField({
+  // onChange: function (data) {
+  //   console.log("onchange name: ", data);
+  // },
+});
+
+myExpiryField.render(cardExpiryContainer);
+
+const myPostalCodeField = cardField.PostalCodeField({
+  minLength: 4,
+  maxLength: 9,
+  // onChange: function (data) {
+  //   console.log("onchange postal: ", data);
+  // },
+});
+
+myPostalCodeField.render(cardPostalCodeContainer);
+
+// console.log("myPostalCodeField");
+// console.log(myNumberField);
+
+// myNumberField.setAttribute('placeholder', 'test');
+setAttributeButton.addEventListener("click", () => {
+  console.log("setAttribute Clicked");
+  cardField.addClass("my-merchant-class");
+});
+
+removeAttributeButton.addEventListener("click", () => {
+  console.log('removeAttribute Clicked');
+  cardField.removeClass("my-merchant-class");
+});
+
+multiCardFieldButton.addEventListener("click", () => {
+  console.log("clicked multi fields button");
+  cardField
+    .submit()
+    .then(() => {
+      console.log("multi card fields submit");
     })
-})
+    .catch((err) => {
+      console.log("There was an error with multi card fields: ", err);
+    });
+});
+
+singleCardFieldButton.addEventListener("click", () => {
+  console.log("clicked single fields button");
+  cardField
+    .submit()
+    .then(() => {
+      console.log("single card fields submit");
+    })
+    .catch((err) => {
+      console.log("There was an error with single card fields: ", err);
+    });
+});
